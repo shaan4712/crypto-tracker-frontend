66bd8a7d110b7d55d38762b8b1b38a726cba2961
Shantanu Atmakuri <shaan2k3@gmail.com>
2024-10-24 14:45:16 +0530

    added header

--- src/components/Common/Footer/index.jsx
+++ src/components/Common/Footer/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div></div>
   )
 }
 
--- src/components/Common/Header/index.jsx
+++ src/components/Common/Header/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './styles.css'
+import * as React from 'react';
+import './styles.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='navbar'>
+      <h1>CryptoTracker<span style={{color: 'var(--blue)'}}>.</span></h1>
+      <div className='links'>
+        <a href='/'>
+          <p className='link'>Home</p>
+        </a>
+        <a href='/'>
+          <p className='link'>Compare</p>
+        </a>
+        <a href='/'>
+          <p className='link'>Watchlist</p>
+        </a>
+        <a href='/'>
+          <p className='link'>Dashboard</p>
+        </a>
+      </div>
+    </div>
   )
 }
 
-export default Header
+export default Header;
