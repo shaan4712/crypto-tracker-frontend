b4d755476ef6f1c2a630d0e6bc4b6341b51ec4df
Shantanu Atmakuri <shaan2k3@gmail.com>
2024-11-06 01:28:05 +0530

    tabs component in dashboard

--- src/components/Dashboard/Tabs/index.jsx
+++ src/components/Dashboard/Tabs/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+// import {Tab} from '@mui/material';
+// import {TabContext, TabList, TabPanel} from '@mui/lab';
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import "./styles.css";
 
-const TabsComponent = () => {
+export default function TabsComponent() {
+  const [value, setValue] = React.useState('grid');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const style = {
+    color: "var(--white)",
+    "& .Mui-selected": {
+      color: "var(--blue) !important",
+    },
+    fontFamily: "Inter,sans-serif",
+    fontWeight: 600,
+    textTransform: "capitalize",
+  };
+
   return (
-    <div>TabsComponent</div>
-  )
-}
+    <div>
+      <TabContext value={value}>
+          <TabList onChange={handleChange} variant="fullWidth">
+            <Tab label="Grid" value="grid" sx={style}/>
+            <Tab label="List" value="list" sx={style}/>
+          </TabList>
 
-export default TabsComponent
+        <TabPanel value="grid">Item One</TabPanel>
+        <TabPanel value="list">Item Two</TabPanel>
+      </TabContext>
+    </div>
+  );
+}
